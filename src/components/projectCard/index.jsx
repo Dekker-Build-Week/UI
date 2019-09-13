bfefa1f06280acfacffe0bd66aab4401dc75f0f5
Nirosan Nadarajah <nirosan.nadarajah@and.digital>
2019-09-13 15:35:28 +0100

    added contributing user icons to homepage project template 😎

--- src/components/projectCard/index.jsx
+++ src/components/projectCard/index.jsx
@@ -1,32 +1,40 @@
-import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
-import clientImage from './bestway.png'
-import styled from 'styled-components';
-
-
+import React from "react";
+import { Card, Image, Icon } from "semantic-ui-react";
+import clientImage from "./bestway.png";
+import styled from "styled-components";
 
 //const customCard = styled(Card)`min-width: 5px;`
 class ProjectCard extends React.Component {
-    render() {
-        return (
-            <Card fluid>
-                <Card.Content>
-                    <Card.Header>The Project</Card.Header>
-                    <Image src={clientImage} floated="right" size="small"></Image>
-                    <Card.Meta>Co-Worker</Card.Meta>
-                    <Card.Description>
-
-                    Matthew is a pianist living in Nashville.
-                    Matthew is a pianist living in Nashville.
-                    Matthew is a pianist living in Nashville.Matthew is a pianist living in Nashville.Matthew is a pianist living in Nashville.Matthew is a pianist living in Nashville.Matthew is a pianist living in Nashville.
-                    Matthew is a pianist living in Nashville.Matthew is a pianist living in Nashville.Matthew is a pianist living in Nashville.Matthew is a pianist living in Nashville.
-                    Matthew is a pianist living in Nashville.Matthew is a pianist living in Nashville.
-
-                    </Card.Description>
-                </Card.Content>
-            </Card>
-        );
-    }
+  render() {
+    return (
+      <Card fluid>
+        <Card.Content>
+          <Card.Header>The Project</Card.Header>
+          <Image src={clientImage} floated="right" size="small"></Image>
+          <Card.Meta>Co-Worker</Card.Meta>
+          <Card.Description>
+            Matthew is a pianist living in Nashville. Matthew is a pianist
+            living in Nashville. Matthew is a pianist living in
+            Nashville.Matthew is a pianist living in Nashville.Matthew is a
+            pianist living in Nashville.Matthew is a pianist living in
+            Nashville.Matthew is a pianist living in Nashville. Matthew is a
+            pianist living in Nashville.Matthew is a pianist living in
+            Nashville.Matthew is a pianist living in Nashville.Matthew is a
+            pianist living in Nashville. Matthew is a pianist living in
+            Nashville.Matthew is a pianist living in Nashville.
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a>
+            <Icon name="user" />
+            <Icon name="user" />
+            <Icon name="user" />
+            <Icon name="user" />
+          </a>
+        </Card.Content>
+      </Card>
+    );
+  }
 }
 
 export default ProjectCard;
