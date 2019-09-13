7cce9915c41ab73eb42aa10bd629412e6ab88d73
Henry Pye <henrypye@outlook.com>
2019-09-13 14:56:24 +0100

    Margin For Grid

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import Menu from './components/menu';
 import Grid from './components/grid';
 import LogoImage from './components/menu/logo.jpg';
+import styled from 'styled-components';
 
+
+const MainContainer = styled.div`width:100%;`;
+const GridContainer = styled.div`margin-left:auto !important; margin-right:auto !important; width:85%;`;
 function App() {
+  
   return (
-    <div>
+    <MainContainer>
       <Menu LogoImage = {LogoImage} position = 'left' />
-      <Grid />
-    </div>
+      <GridContainer>
+        <Grid />
+      </GridContainer>
+    </MainContainer>
   );
 }
 
