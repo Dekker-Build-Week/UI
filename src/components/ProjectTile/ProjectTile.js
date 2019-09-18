92aacc92e8f3ce09755d62afebe91056053d6f59
Nirosan Nadarajah <nirosan.nadarajah@and.digital>
2019-09-18 12:29:46 +0100

    fixed issues regarding project Description 😎

--- src/components/ProjectTile/ProjectTile.js
+++ src/components/ProjectTile/ProjectTile.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import PropTypes from 'prop-types';
-import FittedImage from 'react-fitted-image';
-import { useSpring, animated } from 'react-spring';
+import PropTypes from "prop-types";
+import FittedImage from "react-fitted-image";
+import { useSpring, animated } from "react-spring";
 
 import GridItem from "components/Grid/GridItem.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardFooter from "components/Card/CardFooter.js";
-import CardBody from 'components/Card/CardBody';
+import CardBody from "components/Card/CardBody";
 
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
 
 import { makeStyles } from "@material-ui/core/styles";
 import projectTileStyle from "assets/jss/material-dashboard-react/components/projectTileStyle.js";
 
-import ProjectView from '../ProjectView/ProjectView.js';
-import ANDiPhotoIcon from './ANDiPhotoIcon';
-import TechStackIcon from './TechStackIcon';
+import ProjectView from "../ProjectView/ProjectView.js";
+import ANDiPhotoIcon from "./ANDiPhotoIcon";
+import TechStackIcon from "./TechStackIcon";
 
 const useStyles = makeStyles(projectTileStyle);
 
@@ -36,7 +36,7 @@ const Fade = React.forwardRef(function Fade(props, ref) {
       if (!open && onExited) {
         onExited();
       }
-    },
+    }
   });
 
   return (
@@ -46,112 +46,116 @@ const Fade = React.forwardRef(function Fade(props, ref) {
   );
 });
 
-const ProjectTile = (props) => {
-    const classes = useStyles();
+const ProjectTile = props => {
+  const classes = useStyles();
 
-    const { projectTitle, clientName, projectDescription, team, clientLogo, images, techStacks, isTeam } = props;
-    console.log("THIS IS THE TECH STACK OBJECT");
-    
-    console.log(techStacks);
-    
+  const {
+    projectTitle,
+    clientName,
+    projectDescription,
+    team,
+    clientLogo,
+    images,
+    techStacks,
+    isTeam
+  } = props;
 
-    const [open, setOpen] = React.useState(false);
+  console.log(techStacks);
 
-    const handleOpen = () => {
-      setOpen(true);
-    };
+  const [open, setOpen] = React.useState(false);
 
-    const handleClose = () => {
-      setOpen(false);
-    };
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-    let coverImage = images.find((image) => {
-        return image.position === 0
-    });
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    return (
-        <GridItem xs={12} sm={12} md={4}>
-            <Card>
-              <CardHeader stats icon>
-                <img src = {clientLogo} alt = "Client Logo" className = {classes.clientLogo}/>
-                <p></p>
-                <h3 className={classes.cardTitle}>
-                  {projectTitle}
-                </h3>
-              </CardHeader>
-              <CardBody className = {classes.cardBody}>
-                <FittedImage
-                   fit="cover"
-                   loader={<div>Loading</div>}
-                   onLoad={(...args) => console.log(...args)}
-                   onError={(...args) => console.log(...args)}
-                   src={coverImage.source}
-                 />
-                  <button type="button" onClick={handleOpen}>
-                     Open
-                  </button>
-                 <Modal
-                      aria-labelledby="spring-modal-title"
-                      aria-describedby="spring-modal-description"
-                      className={classes.modal}
-                      open={open}
-                      onClose={handleClose}
-                      closeAfterTransition
-                      BackdropComponent={Backdrop}
-                      BackdropProps={{
-                        timeout: 500
-                      }}
-                    >
-                      <Fade in={open}>
+  let coverImage = images.find(image => {
+    return image.position === 0;
+  });
 
-                        <ProjectView 
-                          projectTitle = {projectTitle} 
-                          clientName = {clientName} 
-                          clientLogo = {clientLogo}
-                          projectDescription = {projectDescription}
-                          techStack = {techStacks}
-                          images = {images}
-                          team = {team}/>
-                      </Fade>
-                    </Modal>
-              </CardBody>
-              <CardFooter stats>
-                <div className={classes.stats}>
-                  <ul className = {classes.ANDiList}>
-                    {
-                      isTeam ?
-                      team.map((andi, index) => {
-                        return (
-                          <ANDiPhotoIcon
-                            key = {index}
-                            ANDiPhoto = {andi.ANDiPhoto} />
-                        )
-                      })
-                      :
-                      techStacks.filter(t => t.important).map((tech, index) => {
-                        return (
-                          <TechStackIcon
-                            key = {index}
-                            techStack = {tech.image} />
-                        )
-                      })
-                    }
-                  </ul>
-                </div>
-              </CardFooter>
-            </Card>
-        </GridItem>
-        );
-}
+  return (
+    <GridItem xs={12} sm={12} md={4}>
+      <Card>
+        <CardHeader stats icon>
+          <img
+            src={clientLogo}
+            alt="Client Logo"
+            className={classes.clientLogo}
+          />
+          <p></p>
+          <h3 className={classes.cardTitle}>{projectTitle}</h3>
+        </CardHeader>
+        <CardBody className={classes.cardBody}>
+          <FittedImage
+            fit="cover"
+            loader={<div>Loading</div>}
+            onLoad={(...args) => console.log(...args)}
+            onError={(...args) => console.log(...args)}
+            src={coverImage.source}
+          />
+          <button type="button" onClick={handleOpen}>
+            Open
+          </button>
+          <Modal
+            aria-labelledby="spring-modal-title"
+            aria-describedby="spring-modal-description"
+            className={classes.modal}
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500
+            }}
+          >
+            <Fade in={open}>
+              <ProjectView
+                projectTitle={projectTitle}
+                clientName={clientName}
+                clientLogo={clientLogo}
+                projectDescription={projectDescription}
+                techStack={techStacks}
+                images={images}
+                team={team}
+              />
+            </Fade>
+          </Modal>
+        </CardBody>
+        <CardFooter stats>
+          <div className={classes.stats}>
+            <ul className={classes.ANDiList}>
+              {isTeam
+                ? team.map((andi, index) => {
+                    return (
+                      <ANDiPhotoIcon key={index} ANDiPhoto={andi.ANDiPhoto} />
+                    );
+                  })
+                : techStacks
+                    .filter(t => t.important)
+                    .map((tech, index) => {
+                      return (
+                        <TechStackIcon key={index} techStack={tech.image} />
+                      );
+                    })}
+            </ul>
+          </div>
+        </CardFooter>
+      </Card>
+    </GridItem>
+  );
+};
 
 ProjectTile.propTypes = {
-  projectTitle : PropTypes.string.isRequired,
-  clientName : PropTypes.string.isRequired,
-  projectDescription : PropTypes.string.isRequired,
-  team : PropTypes.arrayOf(PropTypes.object).isRequired,
-  clientLogo : PropTypes.string.isRequired,
-  images : PropTypes.arrayOf(PropTypes.object),
-  techStackss : PropTypes.arrayOf(PropTypes.object)
-}
+  projectTitle: PropTypes.string.isRequired,
+  clientName: PropTypes.string.isRequired,
+  projectDescription: PropTypes.string,
+  team: PropTypes.arrayOf(PropTypes.object).isRequired,
+  clientLogo: PropTypes.string.isRequired,
+  images: PropTypes.arrayOf(PropTypes.object),
+  techStackss: PropTypes.arrayOf(PropTypes.object)
+};
 
 export default ProjectTile;
--- src/components/ProjectView/ProjectView.js
+++ src/components/ProjectView/ProjectView.js
@@ -1,75 +1,72 @@
-import React from 'react';
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardFooter from "components/Card/CardFooter.js";
-import CardBody from 'components/Card/CardBody';
-import projectViewStyle from 'assets/jss/material-dashboard-react/components/projectViewStyle.js';
-import GridItem from 'components/Grid/GridItem';
-import GridContainer from 'components/Grid/GridContainer';
-import ANDiPhotoIcon from "components/ProjectTile/ANDiPhotoIcon";
-import TechStackIcon from "components/ProjectTile/TechStackIcon";
+import CardBody from "components/Card/CardBody";
+import projectViewStyle from "assets/jss/material-dashboard-react/components/projectViewStyle.js";
+import GridItem from "components/Grid/GridItem";
+import GridContainer from "components/Grid/GridContainer";
 import TechStackIconProject from "./TechStackIconProject";
 import ANDiPhotoIconProject from "./ANDiPhotoIconProject";
 import ImageSlider from "./ImageSlider/ImageSlider";
 
 const useStyles = makeStyles(projectViewStyle);
 
-const ProjectView = (props) => {
-    const classes = useStyles();
+const ProjectView = props => {
+  const classes = useStyles();
 
-    const { projectTitle, clientName, projectDescription, clientLogo, images, team, techStack } = props;
+  const {
+    projectTitle,
+    clientName,
+    projectDescription,
+    clientLogo,
+    team,
+    techStack
+  } = props;
 
-
-    return (
-        <div className = {classes.projectView}>
-            <Card> 
-              <CardHeader stats icon className = {classes.cardHeader}>
-                <img src = {clientLogo} alt = "Client Logo" className = {classes.clientLogo}/>
-                <p className={classes.clientName}>{clientName}</p>
-                <h3 className={classes.projectTitle}>
-                  {projectTitle}
-                </h3>
-              </CardHeader>
-              <CardBody className = {classes.cardBody}>
-                  <div className={classes.projectInfo}>
-                    <GridContainer>
-                      <GridItem xs={12} sm={12} md={8}>
-                          <ImageSlider />
-                      </GridItem>
-                      <GridItem xs={12} sm={12} md={4}>
-                          <p className={classes.projectDescription}>{projectDescription}</p>
-                      </GridItem>
-                    </GridContainer>
-                </div>
-              </CardBody>
-              <CardFooter stats>
-              <ul className = {classes.ANDiList}>
-                {
-                  team.map((andi) => {
-                    return (
-                      <ANDiPhotoIconProject
-                        ANDiPhoto = {andi.ANDiPhoto} />
-                    )
-                  })
-                }
-              </ul>
-              <ul>  {
-
-                  techStack.map((tech) => {
-                    return (
-                      <TechStackIconProject
-                        techStack = {tech.image} />
-                    )
-                  })
-                }
-
-              </ul>
-              </CardFooter>
-            </Card>
-        </div>
-    );
-}
-
+  return (
+    <div className={classes.projectView}>
+      <Card>
+        <CardHeader stats icon className={classes.cardHeader}>
+          <img
+            src={clientLogo}
+            alt="Client Logo"
+            className={classes.clientLogo}
+          />
+          <p className={classes.clientName}>{clientName}</p>
+          <h3 className={classes.projectTitle}>{projectTitle}</h3>
+        </CardHeader>
+        <CardBody className={classes.cardBody}>
+          <div className={classes.projectInfo}>
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={8}>
+                <ImageSlider />
+              </GridItem>
+              <GridItem xs={12} sm={12} md={4}>
+                <p className={classes.projectDescription}>
+                  {projectDescription}
+                </p>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </CardBody>
+        <CardFooter stats>
+          <ul className={classes.ANDiList}>
+            {team.map(andi => {
+              return <ANDiPhotoIconProject ANDiPhoto={andi.ANDiPhoto} />;
+            })}
+          </ul>
+          <ul>
+            {" "}
+            {techStack.map(tech => {
+              return <TechStackIconProject techStack={tech.image} />;
+            })}
+          </ul>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
 
 export default ProjectView;
