b32fa438c6c8fc85b39275ae970e71b71adcdff9
Nirosan Nadarajah <nirosan.nadarajah@and.digital>
2019-10-01 13:48:57 +0100

    Changed Video Player component

--- src/components/ProjectView/ProjectView.js
+++ src/components/ProjectView/ProjectView.js
@@ -4,91 +4,100 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardFooter from "components/Card/CardFooter.js";
-import CardBody from 'components/Card/CardBody';
-import projectViewStyle from 'assets/jss/material-dashboard-react/components/projectViewStyle.js';
-import GridItem from 'components/Grid/GridItem';
-import GridContainer from 'components/Grid/GridContainer';
+import CardBody from "components/Card/CardBody";
+import projectViewStyle from "assets/jss/material-dashboard-react/components/projectViewStyle.js";
+import GridItem from "components/Grid/GridItem";
+import GridContainer from "components/Grid/GridContainer";
 import TechStackIcon from "./TechStackIcon";
 import ANDiPhotoIcon from "./ANDiPhotoIcon";
-import ImageSlider from './ImageSlider/ImageSlider';
+import ImageSlider from "./ImageSlider/ImageSlider";
+import ReactPlayer from "react-player";
 
 const useStyles = makeStyles(projectViewStyle);
 
 const ProjectView = props => {
   const classes = useStyles();
 
-    const { projectTitle, clientName, projectDescription, clientLogo, coverImage, imagePaths, team, techStack, clientVideo } = props;
-    return (
-        <div className = {classes.projectView}>
-            <Card> 
-              <CardHeader stats icon className = {classes.cardHeader}>
-                <img src = {imagePaths+"/"+clientLogo} alt = "Client Logo" className = {classes.clientLogo}/>
-                <p className={classes.clientName}>{clientName}</p>
-                <h3 className={classes.projectTitle}>
-                  {projectTitle}
-                </h3>
-                <div className = {classes.projectBorderTop}></div>
-              </CardHeader>
-              <CardBody className = {classes.projectBody}>
-                  <div className={classes.projectInfo}>
-                    <GridContainer>
-                      <GridItem xs={12} sm={12} md={8}>
-                        <div className = {classes.contentContainer}>
-                          <video width="100%" height="100%" autoPlay>
-                            <source src={imagePaths + "/" + clientVideo} type="video/mp4"/>
-                          </video>
-                        </div>
-                      </GridItem>
-                      <GridItem xs={12} sm={12} md={4}>
-                          <GridContainer>
-                            <GridItem xs ={12} sm = {12} md = {12}>
-                              <div className = {classes.projectDescriptionContainer}>
-                                <p className={classes.projectDescription}>{projectDescription}</p>
-                              </div>
-                            </GridItem>
-                            <GridItem xs = {12} sm = {12} md = {12}> 
-                              <div className = {classes.teamIcons}>                           
-                                <br/>
-                                <h6>Team Members</h6>
-                                <GridContainer>
-                                  {team.map((andi, index) => {
-                                      return (
-                                        <ANDiPhotoIcon 
-                                          key = {index}
-                                          ANDiPhoto = {imagePaths+"/"+andi.photoPath}
-                                          ANDiName = {andi.name}/>
-                                      
-                                  )
-                                  })}    
-                                </GridContainer>
-                              </div> 
-                            </GridItem>
-                          </GridContainer>
-                      </GridItem>
-                    </GridContainer>
+  const {
+    projectTitle,
+    clientName,
+    projectDescription,
+    clientLogo,
+    coverImage,
+    imagePaths,
+    team,
+    techStack,
+    clientVideo
+  } = props;
+  return (
+    <div className={classes.projectView}>
+      <Card>
+        <CardHeader stats icon className={classes.cardHeader}>
+          <img
+            src={imagePaths + "/" + clientLogo}
+            alt="Client Logo"
+            className={classes.clientLogo}
+          />
+          <p className={classes.clientName}>{clientName}</p>
+          <h3 className={classes.projectTitle}>{projectTitle}</h3>
+          <div className={classes.projectBorderTop}></div>
+        </CardHeader>
+        <CardBody className={classes.projectBody}>
+          <div className={classes.projectInfo}>
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={8}>
+                <div className={classes.contentContainer}>
+                  <ReactPlayer url={imagePaths + "/" + clientVideo} playing />
                 </div>
-              </CardBody>
-              <CardFooter className = {classes.projectFooter}>
-              <ul className = {classes.projectANDis}>
-                {
-                  team
-                  ?
-                  techStack.map((tech, index) => {
-                    return (
-                      <TechStackIcon
-                        key = {index}
-                        techStack = {imagePaths+"/"+tech.imagePath} />
-                    )
-                  })
-                  :
-                  null
-                }  
-                </ul>
-            
-              </CardFooter>
-            </Card>
-        </div>
-    );
-}
+              </GridItem>
+              <GridItem xs={12} sm={12} md={4}>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={12}>
+                    <div className={classes.projectDescriptionContainer}>
+                      <p className={classes.projectDescription}>
+                        {projectDescription}
+                      </p>
+                    </div>
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={12}>
+                    <div className={classes.teamIcons}>
+                      <br />
+                      <h6>Team Members</h6>
+                      <GridContainer>
+                        {team.map((andi, index) => {
+                          return (
+                            <ANDiPhotoIcon
+                              key={index}
+                              ANDiPhoto={imagePaths + "/" + andi.photoPath}
+                              ANDiName={andi.name}
+                            />
+                          );
+                        })}
+                      </GridContainer>
+                    </div>
+                  </GridItem>
+                </GridContainer>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </CardBody>
+        <CardFooter className={classes.projectFooter}>
+          <ul className={classes.projectANDis}>
+            {team
+              ? techStack.map((tech, index) => {
+                  return (
+                    <TechStackIcon
+                      key={index}
+                      techStack={imagePaths + "/" + tech.imagePath}
+                    />
+                  );
+                })
+              : null}
+          </ul>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
 
 export default ProjectView;
--- src/components/ProjectView/VideoPlayer/VideoPlayer.js
+++ src/components/ProjectView/VideoPlayer/VideoPlayer.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import videoPlayerStyle from 'assets/jss/material-dashboard-react/components/videoPlayerStyle.js';
-import { Player, LoadingSpinner } from 'video-react';
+import videoPlayerStyle from "assets/jss/material-dashboard-react/components/videoPlayerStyle.js";
+import ReactPlayer from "react-player";
 
 const useStyles = makeStyles(videoPlayerStyle);
 
-export default (props) => {
-    const classes = useStyles();
+export default props => {
+  const classes = useStyles();
 
-    const { videoSource } = props;
+  const { videoSource } = props;
 
-    return (
-        <div className = {classes.videoContainer}>
-            <Player autoPlay className = {classes.videoPlayer} >
-                <source src = { videoSource ? `https://${videoSource}` : "https://media.w3.org/2010/05/sintel/trailer_hd.mp4" } />
-                <LoadingSpinner />
-            </Player>
-        </div>
-    );
-}
+  return (
+    <div className={classes.videoContainer}>
+      <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" playing />
+      <Player autoPlay className={classes.videoPlayer}>
+        <source
+          src={
+            videoSource
+              ? `https://${videoSource}`
+              : "https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+          }
+        />
+        <LoadingSpinner />
+      </Player>
+    </div>
+  );
+};
